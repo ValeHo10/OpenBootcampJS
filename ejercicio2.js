3b849d6253762fb6afcd4c5c1f230faf1eb771c1
Vale <valehonore26@gmail.com>
2022-09-30 18:20:13 -0300

    ejercicio2 terminado

---
+++ ejercicio2.js
@@ -0,0 +1,7 @@
+const lista = ["Valeria", 33, true,  new Date("april 26 1989"), {titulo: "El Principito", autor: "Antoine de Saint-Exupéry", fecha: new Date("april, 6, 1943"), url: "http://www.agirregabiria.net/g/sylvainaitor/principito.pdf"
+}];
+
+console.log(lista);
+
+
+
