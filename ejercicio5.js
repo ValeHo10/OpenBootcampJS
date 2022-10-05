63233ac7985e16e91ad3a54679dbea448c63d5a1
Vale <valehonore26@gmail.com>
2022-10-05 19:29:44 -0300

    ejercicio 5 finalizado

---
+++ ejercicio5.js
@@ -0,0 +1,7 @@
+let alturaCms = 165;
+let alturaMts = 1.65;
+let pesoKg = 55.5;
+let alturaRedondeada = Math.ceil(alturaMts);
+let pesoRedondeado = Math.floor(pesoKg);
+let comparacion = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
